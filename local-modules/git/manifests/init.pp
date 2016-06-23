21acfc46c7f645c217ec3ad1b05965adfebcefff
Matus Goljer <matus.goljer@gmail.com>
2016-06-23 23:06:05 +0200

    Simplify the git ppa declaration

--- local-modules/git/manifests/init.pp
+++ local-modules/git/manifests/init.pp
@@ -1,19 +1,16 @@
 class git {
 
-  apt::ppa { 'ppa:git-core/ppa':
-    options => '-y',
-  }
-  ->
-  apt::key { 'ppa:git-core/ppa':
-    id=>'E1DD270288B4E6030699E45FA1715D88E1DF1F24',
+  include apt
+  include mintppa
+
+  mintppa::ppa { 'ppa:git-core/ppa':
+    id=>'E1DD270288B4E6030699E45FA1715D88E1DF1F24'
   }
   ->
   package { 'git':
     ensure => 'present',
     install_options => ['--force-yes', '-y',],
-    require => [
-      Class['apt::update'],
-    ],
+    require => Class['apt::update'],
   }
 
 }
