bee62559a63e5ee9d32e9d2ea27439c65a829748
Matus Goljer <matus.goljer@gmail.com>
2016-06-24 00:11:14 +0200

    More dev tools

--- local-modules/dev/manifests/init.pp
+++ local-modules/dev/manifests/init.pp
@@ -1,5 +1,26 @@
 class dev {
 
+  package { "build-essential": ensure => installed }
+  package { "dpkg-dev": ensure => installed }
+
+  # c + toolchain
+  package { "make": ensure => installed }
+  package { "autoconf": ensure => installed }
+  package { "automake": ensure => installed }
+  package { "libtool": ensure => installed }
+  package { "gdb": ensure => installed }
+  package { "valgrind": ensure => installed }
+  package { "ccache": ensure => installed }
+
+  # python
+  package { "python-all-dev": ensure => installed } ->
+  package { "python3-all-dev": ensure => installed } ->
+  package { "python-virtualenv": ensure => installed } ->
+  package { "virtualfish":
+    provider => "pip",
+    ensure => installed
+  }
+
   # haskell
   package { "haskell-platform": ensure => installed } ->
   package { "c2hs": ensure => installed } ->
