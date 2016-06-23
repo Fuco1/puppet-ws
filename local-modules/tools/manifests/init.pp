44f590d9937e89bf325d09fd0bea37c8458f65bb
Matus Goljer <matus.goljer@gmail.com>
2016-06-23 23:40:35 +0200

    Add tools

---
+++ local-modules/tools/manifests/init.pp
@@ -0,0 +1,18 @@
+class tools {
+  # avconv
+  package { "libav-tools": ensure => installed } ->
+  package { "libavcodec-extra": ensure => installed }
+
+  # stow
+  package { "stow": ensure => installed }
+
+  # m17n - internalization, support for devanagari rendering
+
+  package { "libm17n-dev": ensure => installed } ->
+  package { "m17n-db": ensure => installed } ->
+  package { "m17n-lib-bin": ensure => installed }
+
+  # input methods
+  package { "ibus": ensure => installed }
+
+}
