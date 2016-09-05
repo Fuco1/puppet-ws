d3415a436643ec7cee05cadd0e5a3f34c9c5050b
Matus Goljer <matus.goljer@gmail.com>
2016-09-05 21:14:01 +0200

    Add cron resources

--- local-modules/desktop/manifests/init.pp
+++ local-modules/desktop/manifests/init.pp
@@ -4,4 +4,14 @@ class desktop {
   include hunspell
 
   package { "trayer": ensure => installed }
+
+  cron { 'org-git-commit':
+    command => '/home/matus/bin/org-git-commit',
+    minute => 0,
+  }
+
+  cron { 'run-getmail':
+    command => '/home/matus/bin/run-getmail',
+    minute => 55,
+  }
 }
