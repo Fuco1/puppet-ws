0b4b23d7676ce917c5b79cdb85e2aea3b3802ac9
Matus Goljer <matus.goljer@gmail.com>
2016-11-12 16:49:24 +0100

    Add more cabal operations

--- local-modules/cabal/manifests/init.pp
+++ local-modules/cabal/manifests/init.pp
@@ -1,17 +1,51 @@
 # TODO: magic dependency on $home and $user
 class cabal {
 
+  #include vcsrepo
   include ::cabal::update
+
+  define cabal (
+    $command = $name,
+    $unless = undef,
+    $cwd = undef) {
+    exec { "cabal $name":
+      command => "cabal $command",
+      environment => "HOME=${home[$user]}",
+      path => ['/bin', '/usr/bin', '/usr/local/bin'],
+      require => Package['haskell-platform'],
+      cwd => $cwd,
+      unless => $unless,
+    }
+  }
 
   define install (
     $flags = '',
     $unless = "ghc-pkg list ${name} | grep ${name}"
     ) {
-    exec { "cabal install $name $flags":
-      environment => "HOME=${home[$user]}",
-      path => ['/bin', '/usr/bin', '/usr/local/bin'],
-      require => Package['haskell-platform'],
+    cabal::cabal { "install $name $flags":
       unless => $unless
     }
+  }
+
+  define compile ($cwd = $name) {
+    cabal::cabal { "configure ${cwd}":
+      command => "configure",
+      cwd => $cwd,
+    } ->
+    cabal::cabal { "install ${cwd}":
+      command => "install",
+      cwd => $cwd,
+    }
+  }
+
+  define fromgit(
+    $source,
+    ) {
+    vcsrepo { $name:
+      ensure   => present,
+      provider => git,
+      source   => $source,
+    } ->
+    cabal::compile { $name: }
   }
 }
