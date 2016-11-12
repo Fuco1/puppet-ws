8e9ca7ee25814396dede74b6c507d264556abab2
Matus Goljer <matus.goljer@gmail.com>
2016-11-12 17:18:51 +0100

    Install wine1.8

--- local-modules/tools/manifests/wine.pp
+++ local-modules/tools/manifests/wine.pp
@@ -7,7 +7,7 @@ class wine {
     id=>'883E8688397576B6C509DF495A9A06AEF9CB8DB0'
   }
   ->
-  package { 'wine':
+  package { 'wine1.8':
     ensure => 'latest',
     install_options => ['--force-yes', '-y',],
   }
