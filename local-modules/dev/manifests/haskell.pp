6f2ef74c217709c1db68a1d7602286df7c7d3faf
Matus Goljer <matus.goljer@gmail.com>
2017-05-27 18:11:31 +0200

    Install haskell-stack from source

--- local-modules/dev/manifests/haskell.pp
+++ local-modules/dev/manifests/haskell.pp
@@ -1,8 +1,14 @@
 class haskell {
 
   package { "haskell-platform": ensure => installed } ->
-  package { "haskell-stack": ensure => installed } ->
   package { "c2hs": ensure => installed } ->
-  package { "hlint": ensure => installed }
+  package { "hlint": ensure => installed } ->
+  exec { 'install-haskell-stack':
+    path => ['/bin', '/usr/bin', '/usr/local/bin'],
+    command => 'wget -qO- https://get.haskellstack.org/ | sh',
+    user => $root,
+    environment => "HOME=${home[$user]}",
+    unless => 'which stack',
+  }
 
 }
