78fdbed9cee415c312a74ff930ec3dc833518a25
Matus Goljer <matus.goljer@gmail.com>
2017-06-11 14:36:41 +0200

    Make twmn manifest runnable in isolation

--- local-modules/desktop/manifests/twmn.pp
+++ local-modules/desktop/manifests/twmn.pp
@@ -1,9 +1,23 @@
 class desktop::twmn {
 
+  include ::apt
+
+  include desktop::files
+  include desktop::users
+
+  include tools::git
+  include tools::docker
+
   package { 'libboost-program-options1.58.0': ensure => installed }
+  package { 'libboost-system1.58.0': ensure => installed }
 
   build::install { 'twmn-build':
-    git => 'git@github.com:Fuco1/twmn-build.git'
+    #git => 'git@github.com:Fuco1/twmn-build.git'
+    git => 'https://github.com/Fuco1/twmn-build.git',
+    require => [
+      Package['libboost-program-options1.58.0'],
+      Package['libboost-system1.58.0'],
+    ],
   }
 
 }
