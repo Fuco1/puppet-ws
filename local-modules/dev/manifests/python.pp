1b1160ded181b5adc1cd4d6e1640c34804e92018
Matus Goljer <matus.goljer@gmail.com>
2017-10-14 21:59:35 +0200

    Add missing python packages

--- local-modules/dev/manifests/python.pp
+++ local-modules/dev/manifests/python.pp
@@ -3,5 +3,7 @@ class dev::python {
   include ::python
 
   ::python::pip { 'virtualfish': pkgname => 'virtualfish' }
+  ::python::pip { 'setuptools': pkgname => 'setuptools' }
+  ::python::pip { 'python-mpd2': pkgname => 'python-mpd2' }
 
 }
