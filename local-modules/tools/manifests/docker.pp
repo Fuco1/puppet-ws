1c06c8530abc0ebac668c1dfea91cec2dd811cc5
Matus Goljer <matus.goljer@gmail.com>
2018-11-15 11:09:56 +0100

    Update docker versions for bionic

--- local-modules/tools/manifests/docker.pp
+++ local-modules/tools/manifests/docker.pp
@@ -5,7 +5,7 @@ class tools::docker {
   apt::source { 'docker':
     location     => 'https://download.docker.com/linux/ubuntu',
     architecture => 'amd64',
-    release      => 'xenial',
+    release      => 'bionic',
     repos        => 'stable',
     key          => {
       id => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
@@ -16,7 +16,7 @@ class tools::docker {
   }
 
   package { 'docker-ce':
-    ensure  => '17.03.2~ce-0~ubuntu-xenial',
+    ensure  => 'latest',
     notify  => Service['docker'],
     require => [
       Apt::Source['docker'],
@@ -28,7 +28,7 @@ class tools::docker {
 
   exec { 'install-docker-compose':
     path    => ['/usr/bin', '/bin'],
-    command => 'curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod 755 /usr/local/bin/docker-compose',
+    command => 'curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose && chmod 755 /usr/local/bin/docker-compose',
     unless  => 'test -f /usr/local/bin/docker-compose',
     require => [
       Package['curl'],
