c603f888d671aa15c44b852fb5d86f53ba7ea25f
Matus Goljer <matus.goljer@gmail.com>
2018-11-15 11:10:42 +0100

    Latest wine is now in official repo

--- local-modules/tools/manifests/wine.pp
+++ local-modules/tools/manifests/wine.pp
@@ -1,14 +1,6 @@
-# Install latest version from ppa instead of ubuntu repository
 class tools::wine {
 
-  mintppa::ppa { 'ppa:ubuntu-wine/ppa':
-    id=>'883E8688397576B6C509DF495A9A06AEF9CB8DB0'
-  }
-  ->
-  package { 'wine1.8':
-    ensure          => 'latest',
-    install_options => ['--force-yes', '-y',],
-  } ->
+  package { 'wine64': ensure => latest } ->
   package { 'winetricks': ensure => latest }
 
 }
